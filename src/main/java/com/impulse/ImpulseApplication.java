f129026d508211a8ec53c10d380bb5c4bdece427
caibw <caibw.123@163.com>
2018-10-25 14:10:24 +0800

    do sample project with initial version.

---
+++ src/main/java/com/impulse/ImpulseApplication.java
@@ -0,0 +1,12 @@
+package com.impulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulseApplication.class, args);
+	}
+}
