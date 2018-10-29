39a2bdb497da53416ddc257d78879d0dfe61403c
caibw <caibw.123@163.com>
2018-10-29 16:24:41 +0800

    add test controller and security properties.

--- src/main/java/com/impulse/ImpulseApplication.java
+++ src/main/java/com/impulse/ImpulseApplication.java
@@ -2,11 +2,13 @@ package com.impulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class ImpulseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImpulseApplication.class, args);
+		SpringApplication.run(ImpulseApplication.class, "--debug");
 	}
 }
