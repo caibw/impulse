561dc6bcd3ea8c94d1f0e6c0f1d611f24a90b925
caibw <caibw.123@163.com>
2018-11-15 00:12:18 +0800

    Add any other two scheduled task in a thread pool with max size 1.

--- src/main/java/com/impulse/task/ScheduledTaskRunner.java
+++ src/main/java/com/impulse/task/ScheduledTaskRunner.java
@@ -11,9 +11,21 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class ScheduledTaskRunner {
   private static final Logger log = LoggerFactory.getLogger(ScheduledTaskRunner.class);
 
-  @Scheduled(fixedRate = 5000)
+  @Scheduled(fixedRate = 5000, initialDelay = 5000)
   public void reportCurrentTime() {
     log.info("The time {}, in fixedRate, and thread name {}", LocalDateTime.now(),
+        Thread.currentThread().getName());
+  }
+
+  @Scheduled(fixedDelay = 5000, initialDelay = 5000)
+  public void reportCurrentTime2() {
+    log.info("The time {}, in fixedDelay, and thread name {}", LocalDateTime.now(),
+        Thread.currentThread().getName());
+  }
+
+  @Scheduled(cron = "5 * * * * ?")
+  public void reportCurrentTime3() {
+    log.info("The time {}, in cron, and thread name {}", LocalDateTime.now(),
         Thread.currentThread().getName());
   }
 
